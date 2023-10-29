57ec8d18ba950d3930c6f19f432aa913aca165b7
RafifaJ <110929163+RafifaJ@users.noreply.github.com>
2023-10-29 14:45:48 +0600

    Add files via upload

---
+++ function_Rafifa/index.test.js
@@ -0,0 +1,34 @@
+const request = require('test');
+const app  = require('./index.js'); 
+
+describe('POST /submit', () => {
+  it('return success message if account is opened successfully', async () => {
+    const response = await request(app)
+      .post('/submit')
+      .send({
+        Name: 'Dali Express',
+        accountNumber: '123456',
+        amount: '2000',
+        date: '2023-10-28',
+        
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Bill payment was succesful!');
+  });
+
+  it('return an error message if there is an error making the bill payment', async () => {
+    app.db=db;
+    app.db.query = jest.fn((query, values, callback) => {
+      callback(new Error('Database Error'));
+    });
+
+    const response = await request(app)
+      .post('/submit')
+      .send({
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Error in making the bill payment :(');
+  });
+}); 
